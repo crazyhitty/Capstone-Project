62548bf495d41eee2799e1f912f2aec38d2a01e4
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-09 11:56:43 +0530

    Update gradle task with params in deploy_feature_integrate_ci.sh

--- .travis/deploy_feature_integrate_ci.sh
+++ .travis/deploy_feature_integrate_ci.sh
@@ -3,7 +3,7 @@
 printf "\n--------------------\nDeploying for feature-integrate_ci branch\n--------------------\n"
 
 # Get app version via gradle task.
-versionName=$(./gradlew -q getVersionName | tail -1)
+versionName=$(./gradlew -q getVersionName -PpredatorStoreFile=${storeFile} -PpredatorStorePassword=${storePass} -PpredatorKeyAlias=${keyAlias} -PpredatorKeyPassword=${keyPass} -PapiKey=${apiKey} -PapiSecret=${apiSecret} -PsearchUrl=${searchUrl} -PxAngoliaAgent=${xAngoliaAgent} -PxAngoliaApplicationId=${xAngoliaApplicationId} -PxAngoliaApiKey=${xAngoliaApiKey} -PisReleaseBuild=true | tail -1)
 
 printf "\n--------------------\nCreating test branch for ${versionName}\n--------------------\n"
 
