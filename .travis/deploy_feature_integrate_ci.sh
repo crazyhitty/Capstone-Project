8b86221a90be72bcbbae8b9787d9594f541028ad
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-10 23:51:13 +0530

    Update script for testing

--- .travis/deploy_feature_integrate_ci.sh
+++ .travis/deploy_feature_integrate_ci.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+commit_test() {
+    printf "\n--------------------\nCreating test branch for ${versionName}\n--------------------\n"
+    git checkout -b "test_branch_${versionName}"
+    #git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+push_git() {
+    git remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/crazyhitty/Capstone-Project.git > /dev/null 2>&1
+    git push origin "test_branch_${versionName}"
+    git push --quiet --set-upstream origin "test_branch_${versionName}"
+}
+
 printf "\n--------------------\nDeploying for feature-integrate_ci branch\n--------------------\n"
 
 # Get app version via gradle task.
 versionName=$(./gradlew -q getVersionName -PpredatorStoreFile=${storeFile} -PpredatorStorePassword=${storePass} -PpredatorKeyAlias=${keyAlias} -PpredatorKeyPassword=${keyPass} -PapiKey=${apiKey} -PapiSecret=${apiSecret} -PsearchUrl=${searchUrl} -PxAngoliaAgent=${xAngoliaAgent} -PxAngoliaApplicationId=${xAngoliaApplicationId} -PxAngoliaApiKey=${xAngoliaApiKey} -PisReleaseBuild=true | tail -1)
 
-printf "\n--------------------\nCreating test branch for ${versionName}\n--------------------\n"
-
-# Create test branch according to version.
-git checkout -b "test_branch_${versionName}"
-git push origin "test_branch_${versionName}"
+setup_git
+commit_test
+push_git
