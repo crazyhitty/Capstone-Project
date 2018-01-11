4db2bf43982238c7b9f702438af829f1690f6dd6
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-12 00:41:46 +0530

    Install xmlstarlet package in deploy_feature_integrate_ci.sh

--- .travis/deploy_feature_integrate_ci.sh
+++ .travis/deploy_feature_integrate_ci.sh
@@ -23,6 +23,9 @@ printf "\n--------------------\nDeploying for feature-integrate_ci branch\n-----
 # Get app version via gradle task.
 versionName=$(./gradlew -q getVersionName -PpredatorStoreFile=${storeFile} -PpredatorStorePassword=${storePass} -PpredatorKeyAlias=${keyAlias} -PpredatorKeyPassword=${keyPass} -PapiKey=${apiKey} -PapiSecret=${apiSecret} -PsearchUrl=${searchUrl} -PxAngoliaAgent=${xAngoliaAgent} -PxAngoliaApplicationId=${xAngoliaApplicationId} -PxAngoliaApiKey=${xAngoliaApiKey} -PisReleaseBuild=true | tail -1)
 
+# Installing xmlstarlet package for xml parsing.
+sudo apt install xmlstarlet
+
 # Get changelog for selected version.
 changelog=$(xmlstarlet sel -t -v '/changelog/changelogversion[@versionName="${versionName}"]' app/src/main/res/raw/changelog.xml  | sed 's/^/* /' | awk '{$1=$1};1' | tail -n +2 | head -n -1)
 
