63564488987f0647e28b942bc9f4041b8000cd4d
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-13 00:57:18 +0530

    Update deploy_master.sh to push version tags whenever code is pushed to master

--- .travis/deploy_master.sh
+++ .travis/deploy_master.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+commit_tag() {
+    printf "\n--------------------\nCreating tag for master branch for ${versionName}\n--------------------\n"
+    printf "Changelog: \n${changelog}"
+    git tag -a "v${versionName}" -m "Changelog:
+
+    ${changelog}"
+}
+
+push_tag() {
+    git remote rm origin
+    git remote add origin https://crazyhitty:${GITHUB_ACCESS_TOKEN}@github.com/crazyhitty/Capstone-Project.git > /dev/null 2>&1
+    git push --quiet --set-upstream origin origin "v${versionName}"
+}
+
 printf "\n--------------------\nDeploying for master branch\n--------------------\n"
 
 # Get app version via gradle task.
-versionName=$(./gradlew -q getVersionName | tail -1)
+versionName=$(./gradlew -q getVersionName -PpredatorStoreFile=${storeFile} -PpredatorStorePassword=${storePass} -PpredatorKeyAlias=${keyAlias} -PpredatorKeyPassword=${keyPass} -PapiKey=${apiKey} -PapiSecret=${apiSecret} -PsearchUrl=${searchUrl} -PxAngoliaAgent=${xAngoliaAgent} -PxAngoliaApplicationId=${xAngoliaApplicationId} -PxAngoliaApiKey=${xAngoliaApiKey} -PisReleaseBuild=true | tail -1)
 
-printf "\n--------------------\nCreating tag for ${versionName}\n--------------------\n"
+# Installing xmlstarlet package for xml parsing.
+sudo apt install xmlstarlet
 
-# Create git tag according to version.
-git tag -a versionName
-git push origin versionName
+# Get changelog for selected version.
+changelog=$(xmlstarlet sel -t -v '/changelog/changelogversion[@versionName="'"$versionName"'"]' app/src/main/res/raw/changelog.xml  | sed 's/^/* /' | awk '{$1=$1};1' | tail -n +2 | head -n -1)
+
+setup_git
+commit_tag
+push_tag
